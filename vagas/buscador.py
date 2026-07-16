3e54f44f351dd40ebcfa350441fc53dac1d43d5b
Alan <67348179+alantbarboza@users.noreply.github.com>
2026-07-16 12:09:29 -0300

    recria navegador entre extratores para reduzir uso de memória

--- vagas/buscador.py
+++ vagas/buscador.py
@@ -9,9 +9,14 @@ from vagas.extratores.empregos import extrair_empregos
 from vagas.extratores.infojobs import extrair_infojobs
 from vagas.extratores.jobs99 import extrair_99jobs
 
+
+from gc import collect
+from psutil import Process
+from os import getpid
+process = Process(getpid())
+
 def buscar_vagas(termos_busca):
     with sync_playwright() as p:
-        browser, context = criar_navegador(p)
         vagas = []
 
         extratores = [
@@ -23,27 +28,45 @@ def buscar_vagas(termos_busca):
             ("99Jobs", extrair_99jobs),
         ] 
 
-        try:
-            for site, extrator in extratores:
-                page = None
+        for site, extrator in extratores:
+            browser = None
+            context = None
+            page = None
+
+            try:
+                browser, context = criar_navegador(p)
+                page = criar_pagina(context)
+                
+                vagas += extrator(page, termos_busca)
+
+            except Exception as e:
+                warning(f"Erro fatal {site}: {e}")
+
+            finally:
+                try:
+                    if page and not page.is_closed():
+                        page.close()
+                except:
+                    pass
 
                 try:
-                    page = criar_pagina(context)
+                    if context:
+                        context.close()
+                except:
+                    pass
 
-                    page.goto("about:blank")
-                    
-                    vagas += extrator(page, termos_busca)
+                try:
+                    if browser:
+                        browser.close()
+                except:
+                    pass
 
-                except Exception as e:
-                    warning(f"Erro fatal {site}: {e}")
+                del page
+                del context
+                del browser
+                collect()
 
-                finally:
-                    if page and not page.is_closed():
-                       page.close()
-
-        finally:
-            context.close()
-            browser.close()
+                info(f"{site} | Memória: {process.memory_info().rss / 1024 / 1024:.1f} MB")
 
     info("Busca de vagas concluída. Validando conteúdo e removendo duplicadas...")
     vagas = remover_duplicadas(vagas)
